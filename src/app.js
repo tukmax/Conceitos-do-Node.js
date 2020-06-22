99ca87a13fcea1c60caae39c482f1174f41f8156
Artur Ferreira <tukmax@gmail.com>
2020-06-22 17:02:26 -0300

    finalizando o desafio02

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,58 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+   return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  repositories.push(repository)
+  return response.json(repository)
+
 });
 
+
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const {title, url, techs } = request.body;
+  const repository = repositories.find(repo=>repo.id==id)
+  if(!repository){
+    return response.status(400).send()
+  }
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  if(!isUuid(id)){
+    return response.status(400).json(id)
+  }
+  const repositoryIndex = repositories.findIndex(repository=> repository.id == id)
+  repositories.splice(repositoryIndex, 1)
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find(repository=>repository.id == id);
+  if(!repository){
+    return response.status(400).send()
+  }
+  repository.likes +=1
+  return response.json(repository)
+ 
 });
 
 module.exports = app;
